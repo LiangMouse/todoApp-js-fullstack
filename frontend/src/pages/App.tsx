047959c52aaa79fdb111b40e84743ab60a518b5a
liangshuang34 <liangshuang34@meituan.com>
2025-06-09 20:35:42 +0800

    feat: 黑暗模式

--- frontend/src/pages/App.tsx
+++ frontend/src/pages/App.tsx
@@ -5,9 +5,17 @@ import { Button } from "@/components/ui/button"
 
 function App() {
   console.log("App")
+  const handleToggleDarkMode = () => {
+    const htmlElement = document.documentElement;
+    if (htmlElement.classList.contains("dark")) {
+      htmlElement.classList.remove("dark");
+    } else {
+      htmlElement.classList.add("dark");
+    }
+  } 
   return (
-    <div className="w-full h-[100vh] bg-red-500">
-    <Allotment className="h-[calc(100vh-2rem)] w-full m-4" >
+    <div className="w-full h-[100vh] bg-black">
+    <Allotment className="h-[calc(100vh-2rem)] w-full dark:bg-red-500" >
       {/* 左侧面板 */}
       <Allotment.Pane minSize={200} maxSize={400} preferredSize={300} className="h-full">
         <div className="h-full bg-card p-4 rounded-lg">
@@ -17,10 +25,10 @@ function App() {
       
       {/* 右侧面板 */}
       <Allotment.Pane>
-        <div className="w-full h-full p-4">主要内容区域</div>
-        <Button>Shadcn UI example</Button>
+        <div className="p-4 dark:invisible">主要内容区域</div>
+        <Button onClick={handleToggleDarkMode} className="float-right w-4 h-4 dark:w-[30vh]" >暗黑模式</Button>
       </Allotment.Pane>
-    </Allotment>
+    </Allotment>  
     </div>
   )
 }
